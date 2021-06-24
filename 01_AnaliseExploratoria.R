a5592c5bb736c05bf9f11179aa51f8fbf64b9896
Luiz Fernando Mello <luizf.mello@hotmail.com>
2021-06-24 01:02:54 -0300

    Update 01_AnaliseExploratoria.R
    Atualização do script de analise exploratoria

--- 01_AnaliseExploratoria.R
+++ 01_AnaliseExploratoria.R
@@ -6,3 +6,50 @@
 # Definir local de trabalho
 setwd("C:/repos/Projeto01/Projeto01_DeteccaoDeFraudesEmAplicacoesMobile")
 getwd()
+
+# Carregar arquivo utils no script
+source("Utils.R")
+
+# Instalação e carga de pacotes
+# install.packages("data.table")
+# install.packages("ggplot2")
+# install.packages("gmodels")
+library(data.table)
+library(ggplot2)
+library(gmodels)
+library(dplyr)
+
+# Carregar o dataset
+# ?fread
+apps <- fread("Datasets/train.csv", sep = ",", header = TRUE)
+indexes <- sample.int(nrow(apps), 2000000)
+apps <- apps[indexes,]
+indexes <- NULL
+
+
+# Visualizar o dataset
+str(apps)
+head(apps)
+
+# Converter variáveis numéricas para fator (Obs: Em nosso conjunto de dados
+# Não tem nenhuma variável numérica)
+variaveis_categoricas <- c("ip", "app", "device",
+                           "os", "channel", "is_attributed")
+apps <- to.factor(apps, variaveis_categoricas)
+
+# Visualizar o dataset
+str(apps)
+summary(apps)
+
+# Teste do Qui-quadrado para verificar relacionamento das variáveis
+# Váriavel IP e app
+chisq.test(apps$ip, apps$app)
+# Resultado: Tem um bom relacionamento
+
+# Váriavel app e device (Target)
+chisq.test(apps$app, apps$device)
+# Resultado: Tem um bom relacionamento
+
+# Váriavel device e is_attributed (Target)
+chisq.test(apps$device, apps$os)
+# Resultado: Tem um bom relacionamento
