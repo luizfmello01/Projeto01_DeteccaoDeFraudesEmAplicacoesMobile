76735e0872a08a50a09f42c82b798d6dab15bacb
Luiz Fernando Mello <luizf.mello@hotmail.com>
2021-06-23 23:24:32 -0300

    Create 01_AnaliseExploratoria.R
    Criado o primeiro script do projeto, análise exploratória de dados

---
+++ 01_AnaliseExploratoria.R
@@ -0,0 +1,8 @@
+### Parte 1: Análise exploratória dos dados ###
+## Nesse parte do script será realizado a compreensão dos dados,
+## Medidas de tendencia central, dispersão, histogramas, boxplots, análise de correlação,
+## tabela de contingência, proporção da categoria, etc.
+
+# Definir local de trabalho
+setwd("C:/repos/Projeto01/Projeto01_DeteccaoDeFraudesEmAplicacoesMobile")
+getwd()
