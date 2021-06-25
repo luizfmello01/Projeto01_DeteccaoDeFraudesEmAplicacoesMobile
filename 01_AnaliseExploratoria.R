e0c35f5471ff83b1843e5776af36d797a8793f7a
Luiz Fernando Mello <luizf.mello@hotmail.com>
2021-06-24 21:11:49 -0300

    Update 01_AnaliseExploratoria.R
    Finalização da Análise exploratória

--- 01_AnaliseExploratoria.R
+++ 01_AnaliseExploratoria.R
@@ -3,34 +3,35 @@
 ## Medidas de tendencia central, dispersão, histogramas, boxplots, análise de correlação,
 ## tabela de contingência, proporção da categoria, etc.
 
-# Definir local de trabalho
+
+#### Definir local de trabalho ####
 setwd("C:/repos/Projeto01/Projeto01_DeteccaoDeFraudesEmAplicacoesMobile")
 getwd()
 
-# Carregar arquivo utils no script
+
+#### Carregar pacotes e arquivos utilitários ####
 source("Utils.R")
-
-# Instalação e carga de pacotes
 # install.packages("data.table")
-# install.packages("ggplot2")
 # install.packages("gmodels")
+# install.packages("performanceEstimation")
 library(data.table)
-library(ggplot2)
 library(gmodels)
-library(dplyr)
 
-# Carregar o dataset
+
+#### Carregar o dataset ####
 # ?fread
-apps <- fread("Datasets/train.csv", sep = ",", header = TRUE)
-indexes <- sample.int(nrow(apps), 2000000)
-apps <- apps[indexes,]
-indexes <- NULL
+apps <- fread("Datasets/train_sample.csv", sep = ",", header = TRUE)
 
+# O dataset tem muitos dados, vamos realizar um split no dataset
+# indexes <- sample.int(nrow(apps), 2000000)
+# apps <- apps[indexes,]
 
 # Visualizar o dataset
 str(apps)
 head(apps)
 
+
+#### Conversão de tipo de variável ####
 # Converter variáveis numéricas para fator (Obs: Em nosso conjunto de dados
 # Não tem nenhuma variável numérica)
 variaveis_categoricas <- c("ip", "app", "device",
@@ -41,7 +42,43 @@ apps <- to.factor(apps, variaveis_categoricas)
 str(apps)
 summary(apps)
 
-# Teste do Qui-quadrado para verificar relacionamento das variáveis
+
+#### Verificar valores NA das variáveis ####
+# IP
+any(is.na(apps$ip))
+# Resultado: Não contem nenhuma variável NA
+
+# APP
+any(is.na(apps$app))
+# Resultado: Não contem nenhuma variável NA
+
+# DEVICE
+any(is.na(apps$device))
+# Resultado: Não contem nenhuma variável NA
+
+# OS
+any(is.na(apps$os))
+# Resultado: Não contem nenhuma variável NA
+
+# CHANNEL
+any(is.na(apps$channel))
+# Resultado: Não contem nenhuma variável NA
+
+# CLICK_TIME
+any(is.na(apps$click_time))
+# Resultado: Não contem nenhuma variável NA
+
+# ATTRIBUTED_TIME
+any(is.na(apps$attributed_time))
+sum(is.na(apps$attributed_time))
+# Resultado: A variável attributed_time tem muitos valores NA
+
+# IS_ATTRIBUTED
+any(is.na(apps$is_attributed))
+# Resultado: Não contem nenhuma variável NA
+
+
+#### Teste do Qui-quadrado para verificar relacionamento das variáveis ####
 # Váriavel IP e app
 chisq.test(apps$ip, apps$app)
 # Resultado: Tem um bom relacionamento
@@ -53,3 +90,10 @@ chisq.test(apps$app, apps$device)
 # Váriavel device e is_attributed (Target)
 chisq.test(apps$device, apps$os)
 # Resultado: Tem um bom relacionamento
+
+# Proporça da variável target (is_attributed)
+prop.table(table(apps$is_attributed))
+# Resultado: As classes estão desbalanceadas
+
+# Verificado que temos duas variáveis de data, não são boas para o modelo
+# de machine learning, transformar em categoria ou retira-la
