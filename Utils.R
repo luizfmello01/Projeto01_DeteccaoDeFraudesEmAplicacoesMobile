04446ec4b063b3c028de57de0177ce030e74a43e
Luiz Fernando Mello <luizf.mello@hotmail.com>
2021-06-27 14:56:48 -0300

    Update Utils.R
    Adicionado novas funções na biblioteca

--- Utils.R
+++ Utils.R
@@ -1,9 +1,29 @@
 ### Biblioteca para esse projeto ###
 
+library(data.table)
+
 # Função para converter as variáveis númericas em Categorias
 to.factor <- function(df, variables) {
   for (variable in variables) {
     df[[variable]] <- as.factor(df[[variable]])
+  }
+  
+  return(df)
+}
+
+# Função para converter as variáveis em catacter
+to.caracter <- function(df, variables) {
+  for (variable in variables) {
+    df[[variable]] <- as.character(df[[variable]])
+  }
+  
+  return(df)
+}
+
+# Função para converter as variáveis para inteiro
+to.integer <- function(df, variables) {
+  for (variable in variables) {
+    df[[variable]] <- as.integer(df[[variable]])
   }
   
   return(df)
@@ -15,4 +35,22 @@ metrics.models <- function(cm) {
     Precisao = cm$byClass[["Precision"]],
     Recall = cm$byClass[["Recall"]],
     F1Score = cm$byClass[["F1"]]))
+}
+
+# Função para converter as variáveis de hora em catacter
+hour.to.factor <- function(variable) {
+  hora <- hour(variable)
+  resultado <- ""
+  
+  if ( hora >= 0 & hora <= 5  ) {
+    resultado <- "Madrugada"
+  } else if( hora >= 6 & hora <= 12  ) {
+    resultado <- "Manhã"
+  } else if ( hora >= 13 & hora <= 18 ) {
+    resultado <- "Tarde"
+  } else {
+    resultado <- "Noite"
+  }
+  
+  return(resultado)
 }
